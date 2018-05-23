e88f79f05ee5d665e23a334b91eedb8a2fc64519
Блиновс <@223_8>
2018-05-23 21:10:00 +0600

    Add project files.

---
+++ Admin.Lib/Module/Provider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Admin.Lib.Module
+{
+    public class Provider
+    {
+        public List<int> Prefix = new List<int>();
+        public string LogoUrl { get; set; }
+        public string NameCompany { get; set; }
+        public double Percent { get; set; }
+
+        public Provider()
+        {
+
+        }
+    }
+}
---
+++ Admin.Lib/ServiceProvider.cs
@@ -0,0 +1,40 @@
+﻿using Admin.Lib.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Admin.Lib
+{
+    public class ServiceProvider
+    {
+        List<Provider> providers = new List<Provider>();
+        public void AddProvider()
+        {
+            Provider provider = new Provider();
+            Console.WriteLine("Введите название компании: ");
+            provider.NameCompany = Console.ReadLine();
+
+            Console.WriteLine("Введите логотип компании: ");
+            provider.LogoUrl = Console.ReadLine();
+
+            Console.WriteLine("Введите процент компании: ");
+            provider.Percent = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите префикс компании: ");
+
+            bool exit = true;
+            int pre = 0;
+            do
+            {
+                exit = Int32.TryParse(Console.ReadLine(), out pre);
+
+                if (exit)
+                    provider.Prefix.Add(pre);
+
+            } while (exit);
+        }
+    }
+}
